85c3b26e6547a0127a7c18025ed667ce1553cbe5
Anjali Rajendran <84264801+AnjaliRaj015@users.noreply.github.com>
2021-09-07 02:33:22 +0530

    Update DAY6.py

--- Tasks/Anjali-Rajendran/DAY6.py
+++ Tasks/Anjali-Rajendran/DAY6.py
@@ -2,20 +2,18 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.applications.vgg16 import VGG16
-from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.applications import DenseNet121
 from keras.layers import GlobalAveragePooling2D, Dense
 from keras.layers import BatchNormalization, Dropout
 from keras.models import Model
 
 from tensorflow.keras.datasets import cifar10
-
 # setting class names
 class_names=['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
 #loading the dataset
 (x_train,y_train),(x_test,y_test)=cifar10.load_data()
 
 # Normalizing Images
-
 x_train=x_train/255.0
 x_train.shape
 x_test=x_test/255.0
@@ -25,7 +23,7 @@ resnet = ResNet50(weights ='imagenet', include_top = False,
                input_shape =(32,32, 3)) 
 resnet.trainable = False
 
-x= res.output
+x= resnet.output
 
 x = GlobalAveragePooling2D()(x)
 
@@ -39,13 +37,12 @@ x = Dropout(0.5)(x)
 
 x = Dense(10, activation ='softmax')(x)
 
-model = Model(res.input, x)
+model = Model(resnet.input, x)
 model.compile(optimizer ='Adam', 
               loss ="sparse_categorical_crossentropy", 
               metrics =["sparse_categorical_accuracy"]) 
 model.summary() 
 model.fit(x_train,y_train, epochs = 5, validation_data = (x_test,y_test))
-
 test_loss, test_accuracy = model.evaluate(x_test, y_test)
 print("Test accuracy: {}".format(test_accuracy))
 
@@ -75,35 +72,11 @@ model.fit(x_train,y_train, epochs = 5, validation_data = (x_test,y_test))
 test_loss, test_accuracy = model.evaluate(x_test, y_test)
 print("Test accuracy: {}".format(test_accuracy))
 
-
-enet = EfficientNetB0(weights ='imagenet', include_top = False, 
+den121 = DenseNet121(weights ='imagenet', include_top = False, 
                input_shape =(32,32, 3)) 
-enet.trainable = False
+den121.trainable = False
 
-
-x = enet.output
-
-x = GlobalAveragePooling2D()(x)
-
-x = BatchNormalization()(x)
-
-x = Dropout(0.5)(x) 
-x = Dense(512, activation ='relu')(x)
-x = BatchNormalization()(x)
-x = Dropout(0.5)(x)
-
-
-x = Dense(10, activation ='softmax')(x)
-
-model = Model(enet.input, x)
-model.compile(optimizer ='Adam', 
-              loss ="sparse_categorical_crossentropy", 
-              metrics =["sparse_categorical_accuracy"]) 
-model.summary() 
-model.fit(x_train,y_train, epochs = 5, validation_data = (x_test,y_test))
-test_loss, test_accuracy = model.evaluate(x_test, y_test)
-print("Test accuracy: {}".format(test_accuracy))
-x = enet.output
+x = den121.output
 
 x = GlobalAveragePooling2D()(x)
 
@@ -117,7 +90,7 @@ x = Dropout(0.5)(x)
 
 x = Dense(10, activation ='softmax')(x)
 
-model = Model(enet.input, x)
+model = Model(den121.input, x)
 model.compile(optimizer ='Adam', 
               loss ="sparse_categorical_crossentropy", 
               metrics =["sparse_categorical_accuracy"]) 
